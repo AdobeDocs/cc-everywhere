afc579272c4cb20b4362cac75d7e0c6d971ee996
Davide Barranca <undavide@gmail.com>
2024-09-19 16:10:23 +0200

    Add tutorial landing page

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,258 +12,273 @@
 
 module.exports = {
   siteMetadata: {
-    template_id: process.env.GATSBY_TEMPLATE_ID ,
+    template_id: process.env.GATSBY_TEMPLATE_ID,
     product_name: process.env.GATSBY_PRODUCT_NAME,
     docs: {
-      title: 'Get credentials',
-      path: '/get-credential/'
+      title: "Get credentials",
+      path: "/get-credential/",
     },
     pages: [
       {
-        title: 'Adobe Express Embed SDK',
-        path: 'https://developer.adobe.com/express/embed-sdk',
+        title: "Adobe Express Embed SDK",
+        path: "https://developer.adobe.com/express/embed-sdk",
       },
       {
-        title: 'Guides',
-        path: '/guides/'
+        title: "Guides",
+        path: "/guides/",
       },
       {
-        title: 'SDK References',
-        menu: [{
-          title: 'v4 Reference',
-          description: 'v4 Reference',
-          path: '/reference/'
-        },{
-          title: 'v3 Reference',
-          description: 'v3 Reference [Deprecated soon]',
-          path: '/v3/reference/'
-        }, {
-         title: 'v2 Reference [Deprecated]',
-         description: 'v2 Reference',
-         path: '/v2/reference/'
-       }, {
-        title: 'v1 Reference [Deprecated]',
-        description: 'v1 Reference',
-        path: '/v1/reference/'
-      }],
+        title: "SDK References",
+        menu: [
+          {
+            title: "v4 Reference",
+            description: "v4 Reference",
+            path: "/reference/",
+          },
+          {
+            title: "v3 Reference",
+            description: "v3 Reference [Deprecated soon]",
+            path: "/v3/reference/",
+          },
+          {
+            title: "v2 Reference [Deprecated]",
+            description: "v2 Reference",
+            path: "/v2/reference/",
+          },
+          {
+            title: "v1 Reference [Deprecated]",
+            description: "v1 Reference",
+            path: "/v1/reference/",
+          },
+        ],
       },
       {
-        title: 'Support',
-        path: '/support/index.md',
+        title: "Support",
+        path: "/support/index.md",
       },
     ],
     subPages: [
       {
-        title: 'Getting Started',
-        path: '/guides/',
-        pages:[
+        title: "Getting Started",
+        path: "/guides/",
+        pages: [
           {
-            title: 'Overview',
-            path: '/guides/',
+            title: "Overview",
+            path: "/guides/",
           },
           {
-            title: 'Quickstart',
-            path: '/guides/quickstart/',
+            title: "Quickstart",
+            path: "/guides/quickstart/",
           },
-        ]
+        ],
       },
       {
-        title: 'Tools',
-        path: '/guides/full_editor/',
-        pages:[
+        title: "Tools",
+        path: "/guides/full_editor/",
+        pages: [
           {
-            title: 'Full editor',
-            path: '/guides/full_editor/',
+            title: "Full editor",
+            path: "/guides/full_editor/",
             pages: [
               {
-                title: 'Create new project',
-                path: '/guides/full_editor/create_project/'
+                title: "Create new project",
+                path: "/guides/full_editor/create_project/",
               },
               {
-                title: 'Edit existing project',
-                path: '/guides/full_editor/edit_project/'
-              }
-            ]
+                title: "Edit existing project",
+                path: "/guides/full_editor/edit_project/",
+              },
+            ],
           },
           {
-            title: 'Modules',
-            path: '/guides/modules/',
+            title: "Modules",
+            path: "/guides/modules/",
           },
           {
-            title: 'Quick actions',
-            path: '/guides/quick_actions/',
+            title: "Quick actions",
+            path: "/guides/quick_actions/",
           },
-        ]
+        ],
       },
       {
-        title: 'Concepts',
-        path: '/guides/concepts/customization.md',
-        pages:[
+        title: "Concepts",
+        path: "/guides/concepts/customization.md",
+        pages: [
           {
-            title: 'UI Customization',
-            path: '/guides/concepts/customization.md'
+            title: "UI Customization",
+            path: "/guides/concepts/customization.md",
           },
           {
-            title: 'Error Handling',
-            path: '/guides/concepts/error-handling.md'
-          }
-        ]
+            title: "Error Handling",
+            path: "/guides/concepts/error-handling.md",
+          },
+        ],
       },
       {
-        title: 'Tutorials',
-        path:'/guides/tutorials/'
-      },
-      {
-        title: 'Submission and review',
-        path: '/guides/review/',
-      },
-      {
-        title: 'Changelog',
-        path: '/guides/changelog/',
-      },
-      {
-        title: 'Troubleshooting',
-        path: '/guides/troubleshooting/faq.md',
-        pages:[
+        title: "Tutorials",
+        path: "/guides/tutorials/",
+        pages: [
           {
-            title: 'FAQ',
-            path: '/guides/troubleshooting/faq.md'
-          }
-        ]
+            title: "Embed SDK integration tutorial",
+            path: "/guides/tutorials/getting-started.md",
+          },
+          {
+            title: "Full Editor tutorial",
+            path: "/guides/tutorials/full-editor.md",
+          },
+        ],
+      },
+      {
+        title: "Submission and review",
+        path: "/guides/review/",
+      },
+      {
+        title: "Changelog",
+        path: "/guides/changelog/",
+      },
+      {
+        title: "Troubleshooting",
+        path: "/guides/troubleshooting/faq.md",
+        pages: [
+          {
+            title: "FAQ",
+            path: "/guides/troubleshooting/faq.md",
+          },
+        ],
       },
       // {
       //   title: 'Community forum',
       //   path: 'https://community.adobe.com/t5/adobe-express-embed-sdk/ct-p/ct-express-embed-sdk?page=1&sort=latest_replies&lang=all&tabid=all',
       // },
       {
-        title: 'v1 Reference',
-        path: '/v1/reference/',
+        title: "v1 Reference",
+        path: "/v1/reference/",
         header: true,
         pages: [
           {
-            title: 'Initialize SDK', 
-            path: '/v1/reference/', 
+            title: "Initialize SDK",
+            path: "/v1/reference/",
           },
           {
-            title: 'Full editor', 
-            path: '/v1/reference/full_editor/'
+            title: "Full editor",
+            path: "/v1/reference/full_editor/",
           },
           {
-            title: 'Quick actions', 
-            path: '/v1/reference/quick_actions/'
+            title: "Quick actions",
+            path: "/v1/reference/quick_actions/",
           },
           {
-            title: 'Shared types', 
-            path: '/v1/reference/shared_types/'
-          }
-        ]
+            title: "Shared types",
+            path: "/v1/reference/shared_types/",
+          },
+        ],
       },
       {
-        title: 'v2 Reference',
-        path: '/v2/reference/',
+        title: "v2 Reference",
+        path: "/v2/reference/",
         header: true,
         pages: [
           {
-            title: 'Initialize SDK', 
-            path: '/v2/reference/', 
+            title: "Initialize SDK",
+            path: "/v2/reference/",
           },
           {
-            title: 'Full editor', 
-            path: '/v2/reference/full_editor/'
+            title: "Full editor",
+            path: "/v2/reference/full_editor/",
           },
           {
-            title: 'Quick actions', 
-            path: '/v2/reference/quick_actions/'
+            title: "Quick actions",
+            path: "/v2/reference/quick_actions/",
           },
           {
-            title: 'Shared types', 
-            path: '/v2/reference/shared_types/'
-          }
-        ]
+            title: "Shared types",
+            path: "/v2/reference/shared_types/",
+          },
+        ],
       },
       {
-        title: 'Overview',
-        path:'/v3/reference/',
+        title: "Overview",
+        path: "/v3/reference/",
       },
       {
-        title: 'Initialize SDK',
-        path:'/v3/reference/initialize/',
+        title: "Initialize SDK",
+        path: "/v3/reference/initialize/",
       },
       {
-        title: 'v3 Reference',
-        path: '/v3/reference/CCEverywhere/',
+        title: "v3 Reference",
+        path: "/v3/reference/CCEverywhere/",
         pages: [
           {
-            title: 'createDesign', 
-            path: '/v3/reference/CCEverywhere/create_design/'
+            title: "createDesign",
+            path: "/v3/reference/CCEverywhere/create_design/",
           },
           {
-            title: 'editDesign', 
-            path: '/v3/reference/CCEverywhere/edit_design/'
+            title: "editDesign",
+            path: "/v3/reference/CCEverywhere/edit_design/",
           },
           {
-            title: 'openQuickAction',
-            path: '/v3/reference/CCEverywhere/quick_actions/',
-          }, 
+            title: "openQuickAction",
+            path: "/v3/reference/CCEverywhere/quick_actions/",
+          },
           {
-            title: 'close',
-            path: '/v3/reference/CCEverywhere/close/',
-          }, 
+            title: "close",
+            path: "/v3/reference/CCEverywhere/close/",
+          },
           {
-            title: 'terminate',
-            path: '/v3/reference/CCEverywhere/terminate/',
-          }, 
-        ]
+            title: "terminate",
+            path: "/v3/reference/CCEverywhere/terminate/",
+          },
+        ],
       },
       {
-        title: 'Types', 
-        path: '/v3/reference/types/'
+        title: "Types",
+        path: "/v3/reference/types/",
       },
       {
-        title: 'Overview',
-        path:'/v3/reference/',
+        title: "Overview",
+        path: "/v3/reference/",
       },
       {
-        title: 'Overview',
-        path:'/reference/',
+        title: "Overview",
+        path: "/reference/",
       },
       {
-        title: 'initialize',
-        path:'/reference/initialize/',
+        title: "initialize",
+        path: "/reference/initialize/",
       },
       {
-        title: 'CCEverywhere',
-        path: '/reference/CCEverywhere/',
-        expanded: 'true',
+        title: "CCEverywhere",
+        path: "/reference/CCEverywhere/",
+        expanded: "true",
         pages: [
           {
-            title: 'editor', 
-            path: '/reference/CCEverywhere/editor/'
+            title: "editor",
+            path: "/reference/CCEverywhere/editor/",
           },
           {
-            title: 'quickAction', 
-            path: '/reference/CCEverywhere/quickAction/'
+            title: "quickAction",
+            path: "/reference/CCEverywhere/quickAction/",
           },
           {
-            title: 'module',
-            path: '/reference/CCEverywhere/miniEditor/',
-          }, 
+            title: "module",
+            path: "/reference/CCEverywhere/miniEditor/",
+          },
           // {
           //   title: 'close',
           //   path: '/reference/CCEverywhere/close/',
-          // }, 
+          // },
           // {
           //   title: 'terminate',
           //   path: '/reference/CCEverywhere/terminate/',
-          // }, 
-        ]
+          // },
+        ],
       },
       {
-        title: 'Types', 
-        path: '/reference/types/'
-      }
-    ]
+        title: "Types",
+        path: "/reference/types/",
+      },
+    ],
   },
   plugins: [`@adobe/gatsby-theme-aio`],
-  pathPrefix: process.env.PATH_PREFIX || '/express/embed-sdk/docs/'
+  pathPrefix: process.env.PATH_PREFIX || "/express/embed-sdk/docs/",
 };
