dbc013b47da0c155a76ecd0cbb10b382fbcf81a0
Davide Barranca <undavide@gmail.com>
2024-10-08 17:12:09 +0200

    Testing

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,33 @@
 exports.createSchemaCustomization = ({ actions }) => {
-  const { createTypes } = actions
+  const { createTypes } = actions;
   createTypes(`
       type SiteSiteMetadata {
         template_id: String
         product_name: String
       }
-    `)
-}
+    `);
+};
+
+exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
+  if (stage === "build-javascript" || stage === "develop") {
+    const config = getConfig();
+    actions.replaceWebpackConfig({
+      ...config,
+      optimization: {
+        ...config.optimization,
+        splitChunks: {
+          chunks: "all",
+          name: false,
+          cacheGroups: {
+            styles: {
+              name: "styles",
+              test: /\.(css|scss)$/,
+              chunks: "all",
+              enforce: true,
+            },
+          },
+        },
+      },
+    });
+  }
+};
