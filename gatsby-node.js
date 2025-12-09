6de01c888c1855aaef5109b0d6d2df92056e12ff
BaskarMitrah <mit44186@adobe.com>
2025-12-09 11:33:28 +0530

    remove gatsby files

--- gatsby-node.js
+++
@@ -1,9 +0,0 @@
-exports.createSchemaCustomization = ({ actions }) => {
-  const { createTypes } = actions
-  createTypes(`
-      type SiteSiteMetadata {
-        template_id: String
-        product_name: String
-      }
-    `)
-}
